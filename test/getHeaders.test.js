6164858f63a2039f5a4fe6619f8831776601cdf2
cesar_nieto <cesar.nieto@on-et.com>
2018-01-08 17:33:02 -0600

    Se hizo el codigo del Test para modulo getHeaders

--- test/getHeaders.test.js
+++ test/getHeaders.test.js
@@ -1,1 +1,19 @@
 let fs = require("fs");
+let expect = require("chai");
+let getHeadersFromFile = require("../src/getHeadersFromFile");
+let fIleDirection = "./data/Header_ex1.txt";
+
+let headerTest = ["RFC_EMISOR",
+    "FECHA",
+    "TOTAL"
+]
+describe.only("The file must be sent and load without errors", () => {
+
+    it("the direction is sent via fileDirection", () => {
+        getHeadersFromFile(fIleDirection).then((headers) => {
+            expect(headers.headers).to.deep.equal(headerTest);
+            expect(headers.order.colum).to.deep.equal(1);
+            expect(headers.order.ascending).to.deep.equal(true);
+        });
+    })
+});
