0a469bb309fc36be40e8cb04014d9697c4d2eac9
eliu.benitez <eliu.benitez>
2018-01-15 12:19:51 -0600

    Se creo modulo main.js. Con sus respectivas funciones start() y processFileList().

--- src/main.js
+++ src/main.js
@@ -1,36 +1,56 @@
 const getLogger = require('./logger');
+const log = getLogger('main');
+const FILESPATH = './data';
+const FILETYPE = '.xml';
+const HEADERSPATH = './data/Headers_ex2.txt'
+const XLSXFILENAME = 'excel.xlsx'
 
-let xmlFileList = require("xmlFileList")
-let path="./ruta";
-let outFile="./ruta";
-let ExcelGenertor = require("eg");
-let excelGen = new ExcelGenertor(outFile); 
-function start(){
-    excelGen.addHeaders();
-    readFiles().then(()=>{
-        ExcelGenertor.saveFile();
+let fileUtils = require('./fileUtils.js');
+let getHeadersFromFile = require('./getHeadersFromFile.js');
+let getRow = require('./getRow.js');
+let xlsxGenerator = require('./xlsxGenerator').xlsxGenerator;
+let Headers;
+let xlsxGen;
+
+function start() {
+    getHeadersFromFile(HEADERSPATH).then(headers => {
+        log.trace('Headers parsed:', headers);
+        Headers = headers;
+        xlsxGen = new xlsxGenerator(XLSXFILENAME, headers);
+        return fileUtils.loadFileList(FILESPATH, FILETYPE);
     })
+        .then(processFileList)
+        .then(() => {
+            log.trace('FileList process finished.');
+            xlsxGen.sortDataByColumn();
+            return xlsxGen.saveData()
+
+        }).then(ok => {
+            log.debug('File saved');
+            return ok;
+        })
+        .catch(err => {
+            log.fatal("Error:", err);
+        })
+
 }
-function readFiles(){
-    return xmlFileList(path).then((fileList)=>{
-        let p = Promise.resolve();
-        fileList.forEach(fileName => {
-            p = p.then( ()=>{ 
-                return process(fileName)
-            })
+
+function processFileList(fileList) {
+    log.trace("Starting fileList Processing:", fileList);
+    let allPromises = fileList.map((file, i, arr) => {
+        log.trace("Starting process with:", file);
+        return fileUtils.readFile(file).then(dataFile => {
+            log.trace("Finished file read:", file, "size:", dataFile.length);
+            return getRow(Headers, dataFile);
+        }, (e) => { log.warn(e); }).then(row => {
+            log.trace("Finished parsing row:", file, "data:", row);
+            xlsxGen.add(row);
+        }).catch((e) => {
+            log.warn("File %s cant be parsed.", file, e);
         });
-        return p;
-    })
-    
-} 
-
-
-function process(fileName){
-    return loadFile(fileName).then((file)=>{
-        return new CFDIParser(file)
-    }).then((fileParser)=>{
-        return getData(fileParser)
-    }).then((data)=>{
-        return ExcelGenertor.add(data);
-    })
+    });
+    return Promise.all(allPromises);
 }
+
+
+start();
