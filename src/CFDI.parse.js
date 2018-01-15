6cf7bf107ef496c3ef35162a617dcc55841fb318
Pablo Cesar Nieto Martinez <cesar.nieto@on-et.com>
2018-01-15 17:40:40 -0600

    Se modifico el metodo load de la clase CFDI.parse como promesa

--- src/CFDI.parse.js
+++ src/CFDI.parse.js
@@ -1,6 +1,7 @@
 let parseString = require("xml2js").parseString;
 const getLogger = require('./logger');
 const log = getLogger("CFDI.parse");
+
 class CFDIParser {
     constructor() {
         this.comprobanteNodo;
@@ -11,22 +12,27 @@ class CFDIParser {
     }
 
     load(CFDItext) {
-        parseString(CFDItext, (error, result) => {
-            if (error) {
-                log.error("Error al parseString", error);
-                return;
-            }
-            this.comprobanteNodo = result["cfdi:Comprobante"];
-            this.comprobante = this.comprobanteNodo.$;
-            this.emisor = this.comprobanteNodo["cfdi:Emisor"][0].$;
-            this.receptor = this.comprobanteNodo["cfdi:Receptor"][0].$;
-            this.impuestos = this.comprobanteNodo["cfdi:Impuestos"][0].$;
-            log.debug("comprobanteNodo", this.comprobanteNodo);
-            log.debug("comprobante", this.comprobante);
-            log.debug("emisor", this.emisor);
-            log.debug("receptor", this.receptor);
-            log.debug("impuestos", this.impuestos);
+        let promise = new Promise((resolve, reject) => {
+            parseString(CFDItext, (error, result) => {
+                if (error) {
+                    log.error("Error al parseString", error);
+                    return;
+                }
+                this.comprobanteNodo = result["cfdi:Comprobante"];
+                this.comprobante = this.comprobanteNodo.$;
+                this.emisor = this.comprobanteNodo["cfdi:Emisor"][0].$;
+                this.receptor = this.comprobanteNodo["cfdi:Receptor"][0].$;
+                this.impuestos = this.comprobanteNodo["cfdi:Impuestos"][0].$;
+                log.debug("comprobanteNodo", this.comprobanteNodo);
+                log.debug("comprobante", this.comprobante);
+                log.debug("emisor", this.emisor);
+                log.debug("receptor", this.receptor);
+                log.debug("impuestos", this.impuestos);
+                
+                resolve(result);
+            });
         });
+        return promise;
     }
 
     _GET_FECHA() {
